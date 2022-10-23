7632f6cd0134f81af624c4a0d48c688996f8c6a4
Ian Adam Naval <790279+ianonavy@users.noreply.github.com>
2022-10-23 18:31:39 -0400

    Add Google login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,25 @@ import type { AppProps } from "next/app";
 
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
 import getTheme from "../lib/theme";
 import React from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
+type MyAppProps = AppProps<{
+  session: Session;
+}>;
+
+function MyApp({ Component, pageProps }: MyAppProps) {
   // const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
   const prefersDarkMode = false;
   return (
-    <ThemeProvider theme={getTheme(prefersDarkMode)}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SessionProvider session={pageProps.session}>
+      <ThemeProvider theme={getTheme(prefersDarkMode)}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
 
