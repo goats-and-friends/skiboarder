4c3273e8c054f9d8f425fb521d028d18a77c2fe6
Ian Adam Naval <790279+ianonavy@users.noreply.github.com>
2022-12-03 16:44:14 -0500

    Add disclaimer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,12 +4,19 @@ import { Copyright } from "./Copyright";
 
 export function Footer() {
   return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      <Link color="inherit" href="/privacy">
-        Privacy
-      </Link>
-      {" | "}
-      <Copyright />
-    </Typography>
+    <>
+      <Typography variant="body2" color="text.secondary" align="center">
+        <Link color="inherit" href="/privacy">
+          Privacy
+        </Link>
+        {" | "}
+        <Copyright />
+      </Typography>
+      <Typography variant="body2" color="text.secondary" align="center">
+        Disclaimer: this website is just for fun and not a commercial
+        enterprise. Participants understand this website organizes a single
+        particular ski trip and is not open to the general public.
+      </Typography>
+    </>
   );
 }
